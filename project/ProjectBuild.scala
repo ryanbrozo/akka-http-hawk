5de60a01281a98a8a984e9dcdcb841b042523b89
rye <ryanbrozo@gmail.com>
2016-07-06 12:30:16 +0800

    = Updated akka, scala, and java versions

--- project/ProjectBuild.scala
+++ project/ProjectBuild.scala
@@ -2,15 +2,15 @@ import sbt.Keys._
 import sbt._
 
 object BuildDependencies {
-  private val AKKA_VERSION = "2.3.12"
-  private val AKKA_STREAMS_VERSION = "2.0-M1"
+  private val AKKA_VERSION = "2.4.4"
+//  private val AKKA_STREAMS_VERSION = "2.0-M1"
   val SPRAY_VERSION = "1.3.1"
 
   // Dependencies
-  val akkaStreams =       "com.typesafe.akka"             %% "akka-stream-experimental"                  % AKKA_STREAMS_VERSION
-  val akkaHttpCore =      "com.typesafe.akka"             %% "akka-http-core-experimental"               % AKKA_STREAMS_VERSION
-  val akkaHttp =          "com.typesafe.akka"             %% "akka-http-experimental"                    % AKKA_STREAMS_VERSION
-  val akkaHttpTestKit =   "com.typesafe.akka"             %% "akka-http-testkit-experimental"            % AKKA_STREAMS_VERSION   % "test"
+  val akkaStreams =       "com.typesafe.akka"             %% "akka-stream"                               % AKKA_VERSION
+  val akkaHttpCore =      "com.typesafe.akka"             %% "akka-http-core"                            % AKKA_VERSION
+  val akkaHttp =          "com.typesafe.akka"             %% "akka-http-experimental"                    % AKKA_VERSION
+  val akkaHttpTestKit =   "com.typesafe.akka"             %% "akka-http-testkit"                         % AKKA_VERSION   % "test"
   val akkaActor =         "com.typesafe.akka"             %% "akka-actor"                                % AKKA_VERSION
   val sprayCaching =      "io.spray"                      %% "spray-caching"                             % SPRAY_VERSION
   val parboiled =         "org.parboiled"                 %% "parboiled-scala"                           % "1.1.7"
@@ -22,13 +22,15 @@ object BuildDependencies {
 object BuildSettings {
   import BuildDependencies._
 
-  val SCALA_VERSION = "2.11.7"
+  val SCALA_VERSION = "2.11.8"
   val APP_VERSION = "0.1"
+  val JAVA_VERSION = "1.8"
 
   lazy val commonSettings = Seq(
     organization        := "com.ryanbrozo",
     scalaVersion        := SCALA_VERSION,
     version             := APP_VERSION,
+    scalacOptions       += s"-target:jvm-$JAVA_VERSION",
     licenses            := Seq("MIT License" -> url("http://www.opensource.org/licenses/mit-license.php")),
     homepage            := Some(url("https://github.com/ryanbrozo/akka-http-hawk/")),
     scmInfo             := Some(ScmInfo(
